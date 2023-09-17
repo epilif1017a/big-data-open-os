02c1f45ec230ab43a713f24129322ad7ca28c357
Carlos Costa <epilif1017a@gmail.com>
2023-09-17 18:38:02 +0100

    Course 2324 (#5)
    * update versions and change allspark to pyspark
    
    * change spark to 3.4.1
    
    * upgrade spark version in client and hs
    
    * add trino support for delta lake native

--- jupyter-spark/dev/launch-jupyter-spark.sh
+++ jupyter-spark/dev/launch-jupyter-spark.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Note: run on the root of this project (big-data-os)
 
-docker run -d -p 8888:8888 -p 4040:4040 -p 4041:4041 --network hadoop --name jupyter jupyter/all-spark-notebook:spark-3.3.0
+docker run -d -p 8888:8888 -p 4040:4040 -p 4041:4041 --network hadoop --name jupyter jupyter/pyspark-notebook:spark-3.4.1
+# if you have mac m1/m2 use: aarch64-spark-3.4.1 as the tag for the image
--- trino/trino.Dockerfile
+++ trino/trino.Dockerfile
@@ -1,3 +1,4 @@
-FROM trinodb/trino
+FROM trinodb/trino:426
 
 COPY trino/conf/hive.properties /etc/trino/catalog/
+COPY trino/conf/delta_lake.properties /etc/trino/catalog/
