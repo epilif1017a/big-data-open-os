184063a561cde481a2e2e7d3943ba7742112abf1
carloscosta <epilif1017a@gmail.com>
2023-09-17 16:39:50 +0100

    change spark to 3.4.1

--- jupyter-spark/dev/launch-jupyter-spark.sh
+++ jupyter-spark/dev/launch-jupyter-spark.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Note: run on the root of this project (big-data-os)
 
-docker run -d -p 8888:8888 -p 4040:4040 -p 4041:4041 --network hadoop --name jupyter jupyter/pyspark-notebook:spark-3.5.0
-# if you have mac m1/m2 use: aarch64-spark-3.5.0 as the tag for the image
+docker run -d -p 8888:8888 -p 4040:4040 -p 4041:4041 --network hadoop --name jupyter jupyter/pyspark-notebook:spark-3.4.1
+# if you have mac m1/m2 use: aarch64-spark-3.4.1 as the tag for the image
